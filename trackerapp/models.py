822bd9ce5b2092f91bc87e7e1893e3d3dea2e7ac
Katerina <yelkaterina@gmail.com>
2023-06-05 15:47:44 +0200

    Add models: ExpenseCategory and Transactions

--- trackerapp/models.py
+++ trackerapp/models.py
@@ -31,5 +31,39 @@ class AccountCategory(models.Model):
     def calculate_total_balance(cls, user):
         total_balance = cls.objects.filter(user=user).aaggregate(total_balance=Sum('balance'))
         return total_balance['total_balance']
+    
+
+class ExpenseCategory(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.category_name
+    
+
+    def get_total_expenses(self):
+        total = 0
+        expense = Transactions.objects.filter(categoty_name=self)
+        for expense in expenses:
+            total += expense.amount
+        return total
 
 
+    def show_category_expenses(self):
+        expenses_list = Transactions.objects.filter(category=self)
+        return expenses_list
+
+
+class Transactions(models.Model):
+    TRANSACTION_TYPE_CHOICE = (('Expense', 'Expense'), ('Income', 'Income'))
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_category = models.ForeignKey(AccountCategory, on_delete=models.CASCADE)
+    expense_category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=7, unique=True, choices=TRANSACTION_TYPE_CHOICE)
+    transaction_date = models.DateField()
+    notes = models.TextField()
+
+   
