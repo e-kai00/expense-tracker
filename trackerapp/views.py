544194bc3fef865dbe59cea5715dfcceea30dc04
Katerina <yelkaterina@gmail.com>
2023-06-07 17:07:06 +0200

    Fix urls.py, move templates and static

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Transactions
+
 
 # Create your views here.
+
+def index(request):
+    transactions = Transactions.objects.all()
+    context = {
+        'transactions': transactions
+    }
+    return render(request, 'trackerapp/index.html', context)
+
+
+def next(request):
+    return HttpResponse("Hello, world. You're here")
