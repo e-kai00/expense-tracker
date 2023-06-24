d68d5310b5523940b5d60401b667c77b9f244f32
Katerina <yelkaterina@gmail.com>
2023-06-24 20:02:52 +0200

    Add docstrings to views.py

--- trackerapp/views.py
+++ trackerapp/views.py
@@ -10,7 +10,14 @@ import datetime
 
 @login_required
 def index(request):
-    # get the current month and year
+    """ 
+    Render user's transactions for the current month.
+
+    Filter transactions based on the current year and month, categorize them 
+    by types (expense/ income), group expenses by categories, calculate 
+    total balance for current month
+    """
+
     now = datetime.datetime.now()
     year = now.year
     month = now.month
@@ -20,7 +27,7 @@ def index(request):
         transaction_date__year=year,
         transaction_date__month=month
     )
-        # group expenses by categories, accumulate income
+    
     total_by_category = transactions.filter(
         transaction_type='Expense'
         ).values('expense_category__category_name'
@@ -29,9 +36,8 @@ def index(request):
 
     total_income = transactions.filter(
         transaction_type='Income'
-        ).aggregate(total=Sum('amount'))['total'] or 0
-        
-        # tatal balance for month
+        ).aggregate(total=Sum('amount'))['total'] or 0        
+    
     balance = 0   
     for transaction in transactions:
         if transaction.transaction_type == 'Income':
@@ -50,6 +56,15 @@ def index(request):
 
 @login_required
 def add_expense(request):
+    """ 
+    Add expense transaction for the current user.
+
+    Allow the user to add an expense transaction by submitting a form with 
+    the required information. The transaction is associated with the current user,
+    and the expense category is determined based on the user's selection. After submission
+    returns to the home page.
+    """
+
     user = request.user       
     if request.method == 'POST':
         form = TransactionForm(request.POST, user=user)
@@ -77,6 +92,14 @@ def add_expense(request):
 
 @login_required
 def add_income(request):
+    """ 
+    Add income transaction for the current user.
+
+    Allow the user to add an income transaction by submitting a form with 
+    the required information. The transaction is associated with the current user. 
+    After submission returns to the home page.
+    """
+
     if request.method == 'POST':
         form = TransactionIncomeForm(request.POST)
         if form.is_valid():            
@@ -91,11 +114,18 @@ def add_income(request):
     return render(request, 'trackerapp/add_income.html', {'form': form})
 
 
-# --------------FILTERS
+# --------------FILTER PAGE
 
 @login_required
 def transactions(request):
-    # filter by month
+    """
+    Filter user's transactions by month and display them.
+
+    Allow the user to filter their transactions based on a selected month. 
+    The transactions are sorted in descending order by transaction date.    
+    By default, the transactions for the current month are displayed.
+    """
+
     year = datetime.datetime.now().year
     month_choices = [
         (1, 'January'), 
@@ -111,7 +141,7 @@ def transactions(request):
         (11, 'November'), 
         (12, 'December')
     ]
-    
+        
     transactions = Transactions.objects.filter(
         user=request.user).order_by(
         F('transaction_date').desc()
@@ -127,24 +157,41 @@ def transactions(request):
         )
     else:
         month = datetime.datetime.now().month
-
+    
+    # display selected month
     for month_num, month_name in month_choices:
         if month_num == month:
             selected_month = month_name
+
+    context = {
+        'transactions': transactions,
+        'month_choices': month_choices,
+        'selected_month': selected_month
+    }
       
-    return render(request, 'trackerapp/transactions.html', {'transactions': transactions, 'month_choices': month_choices, 'selected_month': selected_month})
+    return render(request, 'trackerapp/transactions.html', context)
 
 
 # --------------EXPENSE CATEGORIES
 
 @login_required
 def categories(request):
+    """ Display user's expense categories """
+
     categories = ExpenseCategory.objects.filter(user=request.user)
     return render(request, 'trackerapp/categories.html', {'categories': categories})
 
 
 @login_required
 def categories_add(request):
+    """
+    Create new expense category.
+
+    Allow  the user to create new expense categories by submitting 
+    a form with the required information. After successful submission, 
+    the user is redirected to the previous page.
+    """
+
     user=request.user
     if request.method == 'POST':
         form = AddCategoryForm(request.POST)
@@ -165,6 +212,15 @@ def categories_add(request):
 
 @login_required
 def categories_edit(request, category_id):
+    """
+    Edit expense category.
+
+    Allow the user to update an existing expense category by submitting a form with 
+    the required information. The category to be edited is identified by its unique `category_id`.
+    After successful submission, the user is redirected to the 'categories' page.
+
+    """
+
     category = get_object_or_404(ExpenseCategory, id=category_id, user=request.user)
 
     if request.method == 'POST':
@@ -181,6 +237,13 @@ def categories_edit(request, category_id):
 
 @login_required
 def categories_delete(request, category_id):
+    """ 
+    Delete expense category. 
+
+    Allow the user to delete a category. The category to be deleted is 
+    identified by its unique `category_id`. After successful deletion, 
+    the user is redirected to the 'categories' page.
+    """
     category = get_object_or_404(ExpenseCategory, id=category_id, user=request.user)
     category.delete()
     return redirect('categories')
@@ -190,6 +253,15 @@ def categories_delete(request, category_id):
 
 @login_required
 def expenses_by_category(request):
+    """
+    Retrieve and provide expense data by category for Chart.js.
+
+    Retrieve expense categories associated with the current user and 
+    calculates the total expenses per category. It returns 
+    the data in a format suitable for Chart.js, with category labels and
+    corresponding total expense values.
+    """
+
     user=request.user
     expense_categories = ExpenseCategory.objects.filter(user=user)
 
@@ -199,8 +271,8 @@ def expenses_by_category(request):
     for category in expense_categories:
         transactions = Transactions.objects.filter(user=user, expense_category=category)
         category_total = sum(transaction.amount for transaction in transactions)
-        total_expense.append(category_total)    
-
+        total_expense.append(category_total)
+  
     context = {
         'category_label': category_label,
         'total_expense': total_expense,
