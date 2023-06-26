7cd2795959a0c2dfbdf6dce73dceb62fc056d362
Katerina <yelkaterina@gmail.com>
2023-06-26 11:42:53 +0200

    Update settings.py: static files path

--- tracker/settings.py
+++ tracker/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/trackerapp/static/'
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
